592531b3d93e2703972d838006cf55c8db714cb4
usama2333 <usama.wizz@gmail.com>
2023-02-23 12:15:03 +0500

    fetc data through useeffcet and also implement use callback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState , useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,8 +9,9 @@ function App() {
   const [isLoading , setIsLoading] = useState(false);
   const [error , setError] = useState(null);
 
+  
 
-  async function fetchMoviesHandler() {
+ const fetchMoviesHandler = useCallback ( async () =>  {
     setIsLoading(true);
     setError(null);
 
@@ -44,7 +45,12 @@ function App() {
     }
     setIsLoading(false);
 
-  }
+  } , []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  },[fetchMoviesHandler]);
+  
 
     let content = <p>Found no movies</p>;
 
